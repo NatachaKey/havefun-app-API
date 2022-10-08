99e0c08c1a807816c3258b9edd745daea2d18dcd
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-10-08 13:23:10 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ useEffect(()=>{
 }, [])
 
 const getActivity = async() => {
-  const response= await fetch('http://www.boredapi.com/api/activity/')
+  const response= await fetch('https://www.boredapi.com/api/activity/')
   const data = await response.json();
   setActivity(data);
 }
